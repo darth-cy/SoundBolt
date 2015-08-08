0eb1feec40333a2ac5894555d9158ad40e82ca0d
= <qg231@nyu.edu>
2015-08-08 16:34:21 -0700

    Add backbone and bootstrap

---
+++ app/assets/javascripts/soundbolt.js
@@ -0,0 +1,13 @@
+window.Soundbolt = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Soundbolt.initialize();
+});
