f76a53955dc3e87883c54ec105deb08dc8ad76d8
= <qg231@nyu.edu>
2015-08-09 15:42:08 -0700

    Use version 1 of aws-sdk

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,5 @@
-# Aws.config.update({access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-#            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] })
+AWS.config({access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] })
 
-Aws.config.update({
- region: 'us-west-2',
- credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-})
 
-S3_BUCKET = Aws::S3.new.buckets[ENV['S3_BUCKET']]
+S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
