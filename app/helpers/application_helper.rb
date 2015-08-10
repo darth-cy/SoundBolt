4bbeb35f6ba1a26790e2a55a9e7b596f684d638b
= <qg231@nyu.edu>
2015-08-09 18:44:36 -0700

    Regenerate and fix session controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def insert_authenticity_token
+    html = "<input type='hidden' name='authenticity_token' value='form_authenticity_token'>"
+    html.html_safe
+  end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SessionControllerTest < ActionController::TestCase
+class SessionsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
