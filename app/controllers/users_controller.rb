ce42dbd0e78e2ac0d53fdd908cd02ca83f1e6b27
= <qg231@nyu.edu>
2015-08-09 18:11:55 -0700

    Set up Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+
+  def index
+    # nothing to access here.
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  # No edit and update actions available
+
+  # def edit
+  # end
+  #
+  # def update
+  # end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :avatar)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#index"
   resources :static_pages
+  
+  resources :users
+  resource :session
 end
