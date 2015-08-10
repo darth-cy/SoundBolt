840536a4ac2881bc3da081a428bd461ecf96c4f7
= <qg231@nyu.edu>
2015-08-09 18:05:22 -0700

    Add the user model

---
+++ db/migrate/20150810005956_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :description, null: false
+
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_attachment :users, :avatar
+  end
+end
