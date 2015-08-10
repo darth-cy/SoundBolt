f2d8d50079872d63d0c2b29458c3c7b3e28862e8
= <qg231@nyu.edu>
2015-08-09 18:29:00 -0700

    Write User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,74 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  username            :string           not null
+#  email               :string           not null
+#  description         :string           not null
+#  password_digest     :string           not null
+#  session_token       :string           not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :avatar, attachment_size: { less_than: 5.megabytes }
+
+  has_attached_file :avatar, styles: { thumb: '100x100>', square: '200x200#' }
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    self.password_digest = BCypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    return self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
