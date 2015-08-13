43c20d31dade7919591c045ef1907770cc93ec0d
= <qg231@nyu.edu>
2015-08-12 17:58:04 -0700

    User serializeJSON

--- app/assets/javascripts/views/comment_section.js
+++ app/assets/javascripts/views/comment_section.js
@@ -2,6 +2,10 @@ Soundbolt.Views.CommentSection = Backbone.View.extend({
   template: JST['comment_section'],
   className: "user-view-focus-comment-section container-fluid",
 
+  events: {
+    "submit #new-comment-form", "createComment"
+  }
+
   initialize: function(options){
     this.collection = options.comments;
     this.listenTo(this.collection, 'sync', this.render.bind(this));
@@ -11,5 +15,12 @@ Soundbolt.Views.CommentSection = Backbone.View.extend({
     var content = this.template({ comments: this.collection });
     this.$el.html(content);
     return this;
+  },
+
+  createComment: function(event){
+    event.preventDefault();
+
+    var $form = $(event.currentTarget);
+    var data = $form.serializeJSON();
   }
 })
