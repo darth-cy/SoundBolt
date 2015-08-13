5e9326bc34ff5832f73876a571fa5a44fd695a11
= <qg231@nyu.edu>
2015-08-12 23:41:58 -0700

    Audio bar completed

--- app/assets/javascripts/views/player_main_view.js
+++ app/assets/javascripts/views/player_main_view.js
@@ -3,7 +3,10 @@ Soundbolt.Views.PlayerMainView = Backbone.FusedView.extend({
   template: JST['player_main'],
 
   events:{
-    "click #focus-mode-switch": "switchFocus"
+    "click #focus-mode-switch": "switchFocus",
+    "click #audio-pause-button": "pauseAudio",
+    "click #audio-play-button": "playAudio",
+    "click #audio-seeking-control": "seekAudio"
   },
 
   initialize: function(options){
@@ -15,6 +18,8 @@ Soundbolt.Views.PlayerMainView = Backbone.FusedView.extend({
     }else{
       this.model = new Soundbolt.Models.Track();
     }
+
+    this.progressSyncSchedule = setTimeout(this.updateTime.bind(this), 1000);
 
     this.listenTo(this.model, 'change remove add reset', this.render.bind(this));
     this.listenTo(this.model, 'switch', this.render.bind(this));
@@ -32,9 +37,78 @@ Soundbolt.Views.PlayerMainView = Backbone.FusedView.extend({
     }
   },
 
+  remove: function(){
+    clearInterval(this._syncSchedule);
+  },
+
   render: function(){
+    var thisController = this;
+
     var content = this.template({ track: this.model });
     this.$el.html(content);
+
+    // var audioMaster = document.getElementById("player-master-audio");
+    // audioMaster.addEventListener("timeupdate", thisController.updateTime, false);
+
+    var syncSchedule = setInterval(thisController.updateTime, 1000);
+    this._swapSyncSchedule(syncSchedule);
+
     return this;
+  },
+
+  updateTime: function(){
+    var audioMaster = document.getElementById("player-master-audio");
+    var progress_header = document.getElementById('player-master-inner-progress');
+
+    if(!audioMaster || !progress_header){ console.log("not found"); return; };
+
+    console.log("sync progress!");
+    var $progress_header = $(progress_header);
+    var playPercent = 100 * (audioMaster.currentTime / audioMaster.duration);
+    $progress_header.css("margin-left", playPercent + "%");
+  },
+
+  pauseAudio: function(event){
+    var audioMaster = document.getElementById("player-master-audio");
+    if(!audioMaster.paused){
+      audioMaster.pause();
+    }
+  },
+
+  playAudio: function(event){
+    var audioMaster = document.getElementById("player-master-audio");
+    if(audioMaster.paused){
+      audioMaster.play();
+    }
+  },
+
+  seekAudio: function(event){
+    var audioMaster = document.getElementById("player-master-audio");
+    var seekControl = document.getElementById('audio-seeking-control');
+    var progress_header = document.getElementById('player-master-inner-progress');
+
+    if(!audioMaster || !progress_header || !seekControl){ console.log("not found"); return; };
+
+    var $progressHeader = $(progress_header);
+
+    var newMargLeft = event.pageX - seekControl.offsetLeft;
+
+  	if (newMargLeft >= 0 && newMargLeft <= seekControl.offsetWidth) {
+  		$progressHeader.css("margin-left", newMargLeft + "px");
+  	}
+  	if (newMargLeft < 0) {
+  		$progressHeader.css("margin-left", "0px");
+  	}
+  	if (newMargLeft > seekControl.offsetWidth) {
+  		$progressHeader.css("margin-left", seekControl.offsetWidth + "px");
+  	}
+    var clickPercent = (event.pageX - seekControl.offsetLeft) / seekControl.offsetWidth;
+
+    audioMaster.currentTime = audioMaster.duration * clickPercent;
+  },
+
+  _swapSyncSchedule: function(syncSchedule){
+    this._syncSchedule && clearInterval(this._syncSchedule);
+    this._syncSchedule = syncSchedule;
   }
 })
